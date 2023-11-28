c587eb158fb97da29eaaff1978a40a9ae6bb5ce5
CuongNvcqht3012002 <nguyencuong3012002@gmail.com>
2023-11-28 18:23:18 +0700

    fix

--- dbsetup.js
+++ dbsetup.js
@@ -1,65 +1,70 @@
 const mysql = require('mysql2');
 
-
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'wpr',
   password: 'fit2023',
+  database: 'wpr2023',
+  port: 3306,
 });
-
 
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connected to the database!');
-  connection.query('drop database if exists wpr2023')
 
-  const createDatabase = `CREATE DATABASE IF NOT EXISTS wpr2023;`;
-  connection.query(createDatabase)
-  connection.query('USE wpr2023;')
+  // Drop and create database
+  connection.query('DROP DATABASE IF EXISTS wpr2023;');
+  connection.query('CREATE DATABASE IF NOT EXISTS wpr2023;');
+  connection.query('USE wpr2023;');
 
-  
-  const createUsersTable = `CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(255) NOT NULL UNIQUE,
-    full_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-  )`;
-  
+  // Create users table
+  const createUsersTable = `
+    CREATE TABLE IF NOT EXISTS users (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      username VARCHAR(255) NOT NULL UNIQUE,
+      full_name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL
+    )`;
   connection.query(createUsersTable, (err, result) => {
     if (err) throw err;
     console.log('Users table created!');
   });
 
-  
-  const createEmailsTable = `CREATE TABLE IF NOT EXISTS emails (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT NOT NULL,
-    receiver_id INT NOT NULL,
-    subject VARCHAR(255) NOT NULL,
-    body TEXT NOT NULL,
-    attachment VARCHAR(255),
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
-  )`;
+  // Create emails table
+  const createEmailsTable = `
+    CREATE TABLE IF NOT EXISTS emails (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      sender_id INT NOT NULL,
+      receiver_id INT NOT NULL,
+      subject VARCHAR(255) NOT NULL,
+      body TEXT NOT NULL,
+      attachment VARCHAR(255),
+      is_deleted_by_sender BOOLEAN DEFAULT false,
+      is_deleted_by_receiver BOOLEAN DEFAULT false,
+      created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (sender_id) REFERENCES users(id),
+      FOREIGN KEY (receiver_id) REFERENCES users(id)
+    )`;
   connection.query(createEmailsTable, (err, result) => {
     if (err) throw err;
     console.log('Emails table created!');
   });
 
-  
-  const insertUsers = `INSERT INTO users (user_name, full_name, email, password) VALUES
-  ('user1', 'User One', 'a@a.com', 'password1'),
-  ('user2', 'User Two', 'b@b.com', 'password2'),
-  ('user3', 'User Three', 'c@c.com', 'password3')`;
-
+  // Insert users data
+  const insertUsers = `
+    INSERT INTO users (username, full_name, email, password) VALUES
+    ('user1', 'User One', 'a@a.com', 'password1'),
+    ('user2', 'User Two', 'b@b.com', 'password2'),
+    ('user3', 'User Three', 'c@c.com', 'password3')`;
   connection.query(insertUsers, (err, result) => {
     if (err) throw err;
     console.log('Users data inserted!');
   });
 
-  
-  const insertEmails = `INSERT INTO emails (sender_id, receiver_id, subject, body, attachment) VALUES
+  // Insert emails data
+  const insertEmails = `
+    INSERT INTO emails (sender_id, receiver_id, subject, body, attachment) VALUES
     (1, 2, 'Subject 1', 'Body 1', 'attachment1.pdf'),
     (1, 3, 'Subject 2', 'Body 2', 'attachment2.pdf'),
     (2, 1, 'Subject 3', 'Body 3', NULL),
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const mysql = require('mysql2');
 const ejs = require('ejs');
-const db = require("./dbsetup");
+const multer = require('multer');
 const app = express();
-const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require('path');
-const { error } = require('console');
 const ITEM_PER_PAGE = 5;
 
 const connection = mysql.createConnection({
@@ -17,24 +15,23 @@ const connection = mysql.createConnection({
   port: 3306,
 }).promise();
 
+const upload = multer();
+
 connection.query('USE wpr2023');
-app.use (express.urlencoded({extended: true}));
-app.use (cookieParser());
-app.use (bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
 connection.connect((error) => {
   if (error) {
-    console.error('Error connecting to database: ' + error.stack);
+    console.log('Error connecting to database: ' + error.stack);
     return;
   }
-
   console.log('Connected to database as id ' + connection.threadId);
 });
-
 
 app.get('/', (req, res) => {
   const signInUser = req.cookies.signInUser;
@@ -42,168 +39,116 @@ app.get('/', (req, res) => {
     res.redirect('/inbox');
   }
   else {
-    res.redirect('/signin');
+    res.render('signin', { error: null });
+  }
+});
+
+app.post('/', async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check user and password
+  const query = `SELECT COUNT(*) FROM users WHERE username = ? AND password = ?`;
+
+  try {
+    const [result] = await connection.query(query, [username, password]);
+
+    if (result[0]['COUNT(*)'] === 0) {
+      return res.status(401).render('signin', { error: "Username or password is not correct" });
+    }
+
+    // If both username and password are correct, set a cookie and redirect
+    res.cookie('signInUser', username, { maxAge: 3600000 });
+    res.redirect('/inbox');
+  } catch (error) {
+    console.log('Error in sign-in:', error);
+    res.status(500).render('error', { error: 'Internal Server Error' });
   }
 });
 
 app.get('/signup', (req, res) => {
-  res.render('/signup');
+  res.render('signup', { error: null });
 });
 
-app.get('/signin', (req, res) => {
-  const signInUser = req.cookies.signInUser;
-  if (signInUser){
-    res.redirect('/inbox');
-  } else {
-    res.render ('signin')
-  }
-});
-
-app.post ('/signup', async(req, res) => {
+app.post('/signup', async (req, res) => {
   const full_name = req.body.full_name;
-  const user_name = req.body.user_name;
+  const username = req.body.username;
   const email = req.body.email;
   const password = req.body.password;
-  
-  const confirmPassword = req.body.confirm_password;
-  if (password !== confirmPassword) {
-    return res.status(400).json({ error: 'Password is wrong.' });
-  }
+  const confirmPassword = req.body.confirmPassword;
+
+  if (confirmPassword !== password)
+    return res.status(400).render('signup', { error: 'Re-enter password does not match password' })
+
+  if (password.length < 6)
+    return res.status(400).render('signup', { error: 'The password is too short (less than 6 characters)' })
+
+  // Check if a user with the same email or username already exists
+  const checkUserQuery = 'SELECT COUNT(*) AS userCount FROM users WHERE email = ? OR username = ?';
 
   try {
-    // Tạo một bản ghi người dùng mới
-    const newUser = new User({ full_name, user_name, email, password });
+    const [userCheckResult] = await connection.query(checkUserQuery, [email, username]);
+    const userCount = userCheckResult[0].userCount;
 
-    // Lưu vào cơ sở dữ liệu
-    await newUser.save();
+    if (userCount > 0) {
+      return res.status(400).render('signup', { error: 'User with the same email or username already exists' })
+    }
 
-    res.status(201).json({ message: 'Signup successfully.' });
+    // Create a new user
+    const insertUserQuery = 'INSERT INTO users (full_name, username, email, password) VALUES (?, ?, ?, ?)';
+    await connection.query(insertUserQuery, [full_name, username, email, password]);
+
+    res.redirect('welcome')
   } catch (error) {
+    console.error('Error during signup:', error);
     res.status(500).json({ error: 'Internal Server Error.' });
   }
 });
 
-app.post ('/signin', async (req, res) => {
-  const user_name = req.body.user_name;
-  const password = req.body.password;
-  let missingParams = [];
-  if (user_name === undefined) {
-    missingParams.push('user_name')
-  }
-  if (password === undefined) {
-    missingParams.push('password')
-  }
-  if (missingParams.length > 0) {
-    res.status (400).type("text").send("User's information is invalid")
-  } else {
-    const query = `SELECT COUNT (*) FROM users WHERE users.user_name = '${user_name}'`
-    await connection.query(query).then(([result]) => {
-      if (result[0]['COUNT (*)'] === 0) {
-        res.status (400).type('text').send("Sorry, username does not exist.")
-      }
-    })
-    res.cookie ('signInUser', user_name, {maxAge: 3600000});
-    res.redirect('/inbox');
-  }
+app.get('/welcome', (req, res) => {
+  const signedUpUser = req.cookies.signInUser;
+  res.render('welcome', { username: signedUpUser });
 });
 
-app.get ('/inbox', async (req, res) => {
-  const signInUser = "user1";
-  if (signInUser) {
-    const pageCurrent = parseInt (req.query.page) || 1;
-    try {
-      const elementsResult = await connection.query(
-        `SELECT COUNT (*) as total_elements FROM emails INNER JOIN users ON emails.receiver_id = users.id WHERE users.user_name = '${signInUser}' AND emails.is_deleted_by_receiver = 0;`
-      );
-      const totalElements = elementsResult[0][0].total_elements;
-      const offset = (pageCurrent - 1) * ITEM_PER_PAGE;
-      const result = await connection.query(`
-      SELECT emails.*, sender.id as sender_id, sender.user_name as sender_username, receiver.id as receiver_id, receiver.user_name as receiver_username
-      FROM emails INNER JOIN users AS sender ON emails.sender_id = sender.id
-                  INNER JOIN users AS receiver ON emails. receiver_id = receiver.id
-      WHERE receiver.user_name = '${signInUser}'
-      AND emails.is_deleted_by_receiver = 0
-      ORDER BY emails.created_date DESC
-      LIMIT ${ITEM_PER_PAGE} OFFSET ${offset};`);
-      const inboxMails = result [0];
-      const totalPages = Math.cell (totalElements/ITEM_PER_PAGE);
-      res.render ('inbox', {
-        inboxMails, totalElements, totalPages, pageCurrent,
-      });
-    } catch (error) {
-      console.error ('Fetching inbox data is error:', error);
-      res.status (500).json({error: 'Internal Server Error'});
-    }
-  } else {
-    res.redirect ('/signin');
-  }
-});
-
-app.delete ('/:type/delete', async (req, res) => {
-  const selected_emails = req.body.selected_emails;
-
-  const user_name = "user1";
-  const type = req.params.type;
-  const user_id = await connection.query(`SELECT id FROM users WHERE user_name = '${user_name}'`).then (([result]) => {
-    return result[0].id;
-  });
-  console.log(user_id);
-  const updateColumn = (type === 'sender') ? 'is_deleted_by_sender' : 'is_deleted_by_receiver';
-  const updateQuery = `UPDATE emails SET ${updateColumn} = true WHERE email_id IN (${selected_emails}) AND receiver_id = ${user_id}`;
-  await connection.query (updateQuery) 
-  .then (results => {
-    res.json({message: 'Emails updated successfully'});
-  })
-  .catch (error => {
-    console.error ('Error updating emails:', error);
-    res.status(500).json({error:'Internal Server Error'});
-  });
-});
-
-// Single email detail page (lets user reads an email)
-app.get('/email/:id', async (req, res) => {
-  const id = req.params.id;
-  
-  try {
-    const result = await connection.query(`SELECT * FROM emails WHERE email_id = '${id}'`);
-    const email = result[0];
-
-    res.json(email);
-  } catch (error) {
-    console.error('Failed to retrieve email:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-
-// Outbox page (show sent emails)
-app.get('/outbox', async (req, res) => {
+app.get('/inbox', async (req, res) => {
   const signInUser = req.cookies.signInUser;
 
   if (signInUser) {
+    const pageCurrent = parseInt(req.query.page) || 1;
     try {
-      const elementsResult = await connection.query(`SELECT COUNT(*) as total_elements FROM emails 
-                                                    INNER JOIN users ON emails.sender_id = users.id
-                                                    WHERE users.user_name = '${signInUser}' AND emails.is_deleted_by_sender = 0;`);
+      // Fetch total elements
+      const totalElementsResult = await connection.query(`
+              SELECT COUNT(*) as total_elements
+              FROM emails
+              WHERE receiver_id = (SELECT id FROM users WHERE username = '${signInUser}')
+              AND is_deleted_by_receiver = 0;
+          `);
 
-      const totalElements = elementsResult[0][0].total_elements;
-      const pageCurrent = parseInt(req.query.page) || 1;
+      const totalElements = totalElementsResult[0][0].total_elements;
+
+      // Fetch inbox mails with pagination
       const offset = (pageCurrent - 1) * ITEM_PER_PAGE;
+      const result = await connection.query(`
+              SELECT emails.*, sender.username as sender_username
+              FROM emails
+                  INNER JOIN users AS sender ON emails.sender_id = sender.id
+              WHERE emails.receiver_id = (SELECT id FROM users WHERE username = '${signInUser}')
+                  AND emails.is_deleted_by_receiver = 0
+              ORDER BY emails.created_date DESC
+              LIMIT ${ITEM_PER_PAGE} OFFSET ${offset};
+          `);
 
-      const result = await connection.query(`SELECT emails.*, users.user_name FROM emails 
-                                            INNER JOIN users ON emails.sender_id = users.id 
-                                            WHERE users.user_name = '${signInUser}' AND emails.is_deleted_by_sender = 0 
-                                            LIMIT ${ITEM_PER_PAGE} OFFSET ${offset};`);
-
-      const outboxMails = result[0];
+      const inboxMails = result[0];
       const totalPages = Math.ceil(totalElements / ITEM_PER_PAGE);
 
-      res.render('outbox', {
-        outboxMails, totalElements, totalPages, pageCurrent
+      res.render('inbox', {
+        inboxMails,
+        totalElements,
+        totalPages,
+        pageCurrent,
       });
-
     } catch (error) {
-      console.error('Error fetching outbox data:', error);
+      console.log('Fetching inbox data is error:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   } else {
@@ -211,17 +156,111 @@ app.get('/outbox', async (req, res) => {
   }
 });
 
+app.post('/deleteEmails', async (req, res) => {
+  const signInUser = req.cookies.signInUser;
+  const selectedEmails = req.body.emails;
 
-// Compose page (let user create and send an email to one receiver)
+  try {
+    // Update the database to mark emails as deleted
+    const updateQuery = `
+          UPDATE emails
+          SET is_deleted_by_receiver = 1
+          WHERE receiver_id = (SELECT id FROM users WHERE username = '${signInUser}')
+          AND email_id IN (${selectedEmails.join(',')})
+          AND is_deleted_by_receiver = 0;
+      `;
+
+    const result = await connection.query(updateQuery);
+
+    if (result[0].affectedRows > 0) {
+      res.status(200).json({ success: true });
+    } else {
+      // No rows were affected, meaning the emails were not found or already deleted
+      res.status(404).json({ error: 'Emails not found or already deleted' });
+    }
+  } catch (error) {
+    console.error('Error while deleting emails:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/email/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const result = await connection.query('SELECT * FROM emails WHERE id = ?', [id]);
+    const email = result[0][0]; // Assuming there's only one email with the given id
+
+    if (email) {
+      res.render('emailDetail', {
+        emailSubject: email.subject,
+        emailBody: email.body,
+        attachmentLink: email.attachment,
+      });
+    } else {
+      res.status(404).send('Email not found');
+    }
+  } catch (error) {
+    console.log('Failed to retrieve email:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/outbox', async (req, res) => {
+  const signInUser = req.cookies.signInUser;
+
+  if (signInUser) {
+    try {
+      const elementsResult = await connection.query(`
+              SELECT COUNT(*) as total_elements
+              FROM emails
+              INNER JOIN users ON emails.sender_id = users.id
+              WHERE users.username = '${signInUser}' AND emails.is_deleted_by_sender = 0;
+          `);
+
+      const totalElements = elementsResult[0][0].total_elements;
+      const pageCurrent = parseInt(req.query.page) || 1;
+      const offset = (pageCurrent - 1) * ITEM_PER_PAGE;
+
+      const result = await connection.query(`
+              SELECT emails.*, users.full_name as recipientFullName
+              FROM emails
+              INNER JOIN users ON emails.receiver_id = users.id
+              WHERE emails.sender_id = (SELECT id FROM users WHERE username = '${signInUser}')
+              AND emails.is_deleted_by_sender = 0
+              LIMIT ${ITEM_PER_PAGE} OFFSET ${offset};
+          `);
+
+      const outboxMails = result[0];
+      const totalPages = Math.ceil(totalElements / ITEM_PER_PAGE);
+
+      res.render('outbox', {
+        outboxMails,
+        totalElements,
+        totalPages,
+        pageCurrent,
+      });
+
+    } catch (error) {
+      console.log('Error fetching outbox data:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.redirect('/');
+  }
+});
+
 app.get('/compose', async (req, res) => {
   const signedInUser = req.cookies.signInUser;
 
   if (signedInUser) {
     try {
-      const result = await connection.query(`SELECT email, id FROM users;`);
-      const listOfUsers = result[0];
-      res.render('compose');
+      const result = await connection.query(`SELECT username FROM users WHERE username != '${signedInUser}';`);
+      const recipients = result[0].map(user => user.username);
+
+      res.render('compose', { recipients });
     } catch (error) {
+      console.error('Error fetching recipients:', error);
       res.status(500).send('Internal Server Error');
     }
   } else {
@@ -229,59 +268,48 @@ app.get('/compose', async (req, res) => {
   }
 });
 
+app.post('/compose', upload.none(), async (req, res) => {
+  const { subject, body, recipient } = req.body;
+  const senderUsername = req.cookies.signInUser;
 
-app.post('/compose', async (req, res) => {
-  const subject = req.body.subject;
-  const content = req.body.content;
-  const receiver_email = req.body.receiver_email;
-
-  const sender = "user1";
-  let missingParams = [];
-
-  if (subject === undefined) {
-    missingParams.push('subject');
-  }
-  if (content === undefined) {
-    missingParams.push('content');
-  }
-  if (receiver_email === undefined) {
-    missingParams.push('receiver_email');
+  if (!subject && !body && !recipient) {
+    return res.status(400).type("text").send("Invalid email information! Missing parameters: subject, body, recipient");
   }
 
-  if (missingParams.length > 0) {
-    res.status(400).type("text").send(`Invalid email information! Missing parameters: ${missingParams}`);
-  } else {
-    let receiver_id, sender_id;
+  try {
+    // Fetch sender_id
+    const [senderResult] = await connection.query(`SELECT id FROM users WHERE username = ?`, [senderUsername]);
 
-    try {
-      const receiverResult = await connection.query(`SELECT id FROM users WHERE email = '${receiver_email}'`);
-      receiver_id = receiverResult[0].id;
-      console.log(receiver_id);
+    const senderId = senderResult[0]?.id;
 
-      const senderResult = await connection.query(`SELECT id FROM users WHERE user_name = '${sender}'`);
-      sender_id = senderResult[0].id;
-      console.log(sender_id);
-
-      if (receiver_id && sender_id) {
-        const insert_query = `INSERT INTO emails (subject, content, receiver_id, sender_id) VALUES ('${subject}', '${content}', '${receiver_id}', '${sender_id}')`;
-
-        await connection.query(insert_query);
-        res.redirect('/outbox');
-      }
-    } catch (error) {
-      console.error('Failed to send email:', error);
-      res.status(500).send('Internal Server Error');
+    if (!senderId) {
+      return res.status(400).type("text").send("Invalid sender information");
     }
+
+    // Fetch receiver_id
+    const [recipientResult] = await connection.query(`SELECT id FROM users WHERE username = ?`, [recipient]);
+
+    const recipientId = recipientResult[0]?.id;
+
+    if (!recipientId) {
+      return res.status(400).type("text").send("Invalid recipient information");
+    }
+
+    // Insert email into the database
+    const insertQuery = `
+      INSERT INTO emails (subject, body, sender_id, receiver_id)
+      VALUES (?, ?, ?, ?)
+    `;
+
+    await connection.query(insertQuery, [subject || '(no subject)', body, senderId, recipientId]);
+
+    res.redirect('/outbox');
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
-
-
-// Send email
-app.post('/send', (req, res) => {
-});
-
-// Sign-out feature
 app.get('/signout', (req, res) => {
   res.send('Sign-out page');
 });
